c394105b211793c7aa25015dc32eb4b1d4114e98
just4x3l <axel.engelbosch@hotmail.com>
2022-12-04 13:33:46 +0100

    Rename files

--- day4/part1.py
+++ day4/part1.py
@@ -8,13 +8,13 @@ def main():
         [first_elf_range, second_elf_range] = line.strip().split(",")
         [first_elf_start, first_elf_end] = map(lambda e: int(e), first_elf_range.split("-"))
         [second_elf_start, second_elf_end] = map(lambda e: int(e), second_elf_range.split("-"))
-        if overlap(first_elf_start, first_elf_end, second_elf_start, second_elf_end):
+        if full_overlap(first_elf_start, first_elf_end, second_elf_start, second_elf_end):
             amount += 1
     print(amount)
 
 
-def overlap(start1, end1, start2, end2):
-    return max(start1, start2) <= min(end1, end2)
+def full_overlap(start1, end1, start2, end2):
+    return (start1 <= start2 and end1 >= end2) or (start2 <= start1 and end2 >= end1)
 
 
 if __name__ == '__main__':
